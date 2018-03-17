8ddbdd696ddd433a73c7369e0a1db585d582f2db
xyu6 <yu.bill@hotmail.com>
2018-03-17 09:15:47 +0800

    update

---
+++ res/get_index_new.py
@@ -0,0 +1,11 @@
+import pyaudio
+
+p = pyaudio.PyAudio()
+
+count = p.get_device_count()
+devices = []
+for i in range(count):
+        devices.append(p.get_device_info_by_index(i))
+
+for i, dev in enumerate(devices):
+        print "%d - %s" % (i, dev['name'])
