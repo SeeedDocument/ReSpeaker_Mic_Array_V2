a511b3efe6806e6b7433c6f6d9b050796e411b12
xyu6 <yu.bill@hotmail.com>
2019-06-25 11:45:00 +0800

    correct the code error

--- res/DOA.py
+++ res/DOA.py
@@ -4,10 +4,10 @@ import usb.util
 import time
 
 dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
-#print dev
+
 if dev:
     Mic_tuning = Tuning(dev)
-    print Mic_tuning.direction()
+    print Mic_tuning.direction
     while True:
         try:
             print Mic_tuning.direction
